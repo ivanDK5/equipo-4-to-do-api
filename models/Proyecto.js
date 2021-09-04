74712cdb31b4b5e653c741dd54caaceb51fe2e4f
Zaire <raffexrivaille@gmail.com>
2021-09-04 14:32:52 -0500

    Zaire: Clases, Rutas, y Modelos de Proyecto y Tareas

--- models/Proyecto.js
+++ models/Proyecto.js
@@ -1,13 +1,13 @@
 
 /** Clase que representa un proyecto de trabajo y sus atributos*/
 class Proyecto {
-    constructor(id, titulo, descripcion, fecha_creacion, fecha_modificacion, fecha_termino) {
+    constructor(id, titulo, descripcion, fechaCreacion, fechaModificacion, fechaTermino) {
         this.id = id;
         this.titulo = titulo;
         this.descripcion = descripcion;
-        this.fecha_creacion = fecha_creacion;
-        this.fecha_modificacion = fecha_modificacion;
-        this.fecha_termino = fecha_termino;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaModificacion = fechaModificacion;
+        this.fechaTermino = fechaTermino;
       }
   }
   module.exports = Proyecto;
