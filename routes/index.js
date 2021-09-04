c6696ef0cb1a799a6e0774069218a96b4f192cde
ivanDK5 <ing.iglds@gmail.com>
2021-09-04 17:54:24 -0500

    feat:Added user route to principal file

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,17 @@
 
+// importamos las dependencias necesarias
+var router = require('express').Router();
+
+// definimos el comportamiento en la raíz del endpoint
+router.get('/', (req, res)=>{
+  res.send('Bienvenido a To-Do');
+});
 
 
 router.use('/usuarios',require('./usuarios'));
+router.use('/tareas', require('./tareas'));
+router.use('/proyectos', require('./proyectos'));
+
+// exportamos nuestro nuevo router
+module.exports = router;
+
