ac0c81af6405a914617390c4dc3b28405ebf7c8e
ivanDK5 <ing.iglds@gmail.com>
2021-09-04 19:31:32 -0500

    feat:Added listen port server

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.use(bodyParser.json());
 
 //Configurando las rutas
 app.use('/v1', require('./routes'));
-
+const PORT = 4001;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 //Primero se crea un modelo,luego se crea un controlador, luego un route,luego se agrega al index
