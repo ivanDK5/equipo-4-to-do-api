a1a73dfcbd2f551d8a2b1c2704250a699c60217a
ivanDK5 <ing.iglds@gmail.com>
2021-09-21 14:58:55 -0500

    feat:Added enviroment variables

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-// Express
+if(process.env.NODE_ENV!=='production'){
+  require('dotenv').config();
+}
+
+const mongoose=require('mongoose');
 const express = require('express');
 const app = express();
 
@@ -7,10 +11,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//cdatabase connection
+
+mongoose.connect(process.env.DB_URI);
+mongoose.set('debug',true);
+
 //Configurando las rutas
 app.use('/v1', require('./routes'));
-const PORT = 4001;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
 });
 //Primero se crea un modelo,luego se crea un controlador, luego un route,luego se agrega al index
