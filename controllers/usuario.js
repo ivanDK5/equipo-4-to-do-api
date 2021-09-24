96d13505ed5823ebe7d88ca306c0ae5d73be7f67
ivanDK5 <ing.iglds@gmail.com>
2021-09-23 21:44:20 -0500

    feat:Added user controller

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,25 +1,44 @@
-const Usuario= require('../models/Usuario');
+const mongoose =require('mongoose');
+const Usuario =mongoose.model('Usuario');
 
-
-function crearUsuario(req, res){
-  res.send('crearUsuario');
+function obtenerUsuarios(req, res,next){
+  Usuario.find()
+  .then(user=>{
+    user=user.map(user=>user.publicData())
+    res.status(200).send(user)
+  })
+  .catch(next);
 }
 
-function obtenerUsuarios(req, res){
-  res.send('obtenerUsuario');
+function obtenerUsuario(req,res,next){
+  Usuario.findById(req.params.id)
+  .then(user=> {
+    if(!user){
+      return sendStatus(401)
+    }
+    return res.json(user.publicData())
+  })
+  .catch(next)
 }
 
-function editarUsuario(req, res){
-  res.send('editarUsuario');
+function editarUsuario(req, res,next){
+  Usuario.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+  .then(user=>{
+    res.status(200).json({data:user.publicData(),msj:'Usuario modificado exitosamente'})
+  }).catch(next)
+} 
+
+function eliminarUsuario(req, res,next){
+  Usuario.findByIdAndDelete({_id: req.usuario.id})
+  .then(user=>res.status(200).json({data:user.publicData(),msj:'Usuario modificado exitosamente'}))
+  .catch(next)
 }
 
-function eliminarUsuario(req, res){
-  res.send('eliminarUsuario');
-}
+
 
 module.exports={
-  crearUsuario,
   obtenerUsuarios,
+  obtenerUsuario,
   editarUsuario,
   eliminarUsuario
 }
