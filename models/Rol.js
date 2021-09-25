061ea139f742500b51060a94c6a41812b6716c81
ivanDK5 <ing.iglds@gmail.com>
2021-09-24 21:50:39 -0500

    chore: Rename Rol Schema

--- models/Rol.js
+++ models/Rol.js
@@ -1,14 +1,14 @@
 const mongoose =require('mongoose');
-const rolSchema =new mongoose.Schema({
+const RolSchema =new mongoose.Schema({
   nombre:{type:String,required:true},
   descripcion:{type:String,required:true}
 },{collection:'roles',timestamps:true});
 
-rolSchema.methods.publicData=function() {
+RolSchema.methods.publicData=function() {
   
   const {_id,nombre,descripcion}=this.toObject();
   
   return {nombre:nombre,descripcion:descripcion,id:_id};
 }
 
-mongoose.model('Rol',rolSchema);
+mongoose.model('Rol',RolSchema);
