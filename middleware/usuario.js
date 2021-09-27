81557c733c0b7901362343447c5943e6b60eb46d
ivanDK5 <ing.iglds@gmail.com>
2021-09-26 19:48:59 -0500

    feat:Added new feature to support response

--- middleware/usuario.js
+++ middleware/usuario.js
@@ -1,25 +1,27 @@
 const mongoose=require('mongoose');
+const { filters } = require('../resources/filters');
 const Usuario=mongoose.model('Usuario');
 
-module.exports=(req,res,next)=>{ 
+
+module.exports=(req,res,next)=>{ console.log(req.query);
   if(req.query){
-    let band=[];
-      console.log(req.query);
-      
-      for(field in req.query){
-        band.push(Usuario.isFiltersAllowed(field));
-        
-      }
-      if(!band.includes(false)){
-       
-       return next()
+    let band=[];  
+      if(Object.keys(req.query).length!==0){
+        for(field in req.query){
+         band.push(Usuario.filtersAllowed(field));
+        }
+        if(!band.includes(false)){
+          return next()
+         }else{
+           return res.status(400).send({
+             status:"400",
+             type:"Bad request",
+             msj:"Filter its not supported by this endpoint",
+             filters:`Filters allowed are: ${Usuario.filtersAllowed().toString()}`
+           })
+         }
       }else{
-        return res.status(400).send({
-          status:"400",
-          type:"Bad request",
-          msj:"Filter its not supported by this endpoint",
-          filters:`Filters allowed are: ${Usuario.filtersAllowed().toString()}`
-        })
+        next();
       }
   }else{
    return next()
